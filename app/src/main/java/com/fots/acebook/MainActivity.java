c9566290f884705df4072ffb5d999a4d82a4bafa
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-06-25 11:45:43 +0100

    cleanup

--- app/src/main/java/com/fots/acebook/MainActivity.java
+++ app/src/main/java/com/fots/acebook/MainActivity.java
@@ -36,67 +36,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//
-//
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//
-//        final DatabaseReference ref = database.getReference("/");
-//
-//        DatabaseReference dataRef = ref.child("data");
-//        dataRef.setValue("I'm writing data", new DatabaseReference.CompletionListener() {
-//            @Override
-//            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
-//                if (databaseError != null) {
-//                    System.out.println("Data could not be saved " + databaseError.getMessage());
-//                } else {
-//                    System.out.println("Data saved successfully.");
-//                }
-//            }
-//        });
-//
-//        Button submitBtn = (Button) findViewById(R.id.postSubmit);
-
-//
-//        submitBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            EditText body = (EditText) findViewById(R.id.postBody);
-//
-//
-//            FirebaseDatabase database = FirebaseDatabase.getInstance();
-//
-//            final DatabaseReference myRef = database.getReference("/posts");
-//
-//            String key = myRef.child("posts").push().getKey();
-//            String currentDateTime = DateFormat.getDateTimeInstance().format(new Date());
-//
-//            Post post = new Post(body.getText().toString(), currentDateTime);
-//
-//            myRef.child(key).setValue(post);
-//            Log.i(TAG, myRef.toString());
-//
-//            Intent showListPostsActivity = new Intent(getApplicationContext(), ListPostsActivity.class);
-//            startActivity(showListPostsActivity);
-//
-//            }
-//        });
-
-
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-
-
         Button loginButton = (Button) findViewById(R.id.loginButton);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                List<AuthUI.IdpConfig> providers = Arrays.asList(
-                        new AuthUI.IdpConfig.EmailBuilder().build()
-                );
-
-                startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), RC_SIGN_IN);
+                requestLogin();
             }
         });
 
@@ -113,12 +58,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build()
-        );
-
-        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), RC_SIGN_IN);
-
+        requestLogin();
     }
 
     @Override
@@ -137,25 +77,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+    private void requestLogin() {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build()
+        );
+
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), RC_SIGN_IN);
+    }
+
 }
