10673ba17fb5d714ccd3f2cea8dee57b8e6b95ce
Piers Thompon <pthom198@MGC000009767.warley.ford.com>
2019-06-27 10:21:18 +0100

    changed logout and messenger icons on update page

--- app/src/main/java/com/fots/acebook/UpdatePostActivity.java
+++ app/src/main/java/com/fots/acebook/UpdatePostActivity.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.fots.acebook.models.Post;
@@ -26,9 +30,10 @@ public class UpdatePostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_post);
 
-        Button updateBtn = (Button) findViewById(R.id.postUpdate);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        Button logoutButton = (Button) findViewById(R.id.logoutButton);
+        Button updateBtn = findViewById(R.id.postUpdate);
 
         EditText body = findViewById(R.id.updateBody);
 
@@ -37,12 +42,7 @@ public class UpdatePostActivity extends AppCompatActivity {
         String postId = intent.getStringExtra(PostAdapter.POST_ID);
         body.setText(postBody);
 
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                requestLogout();
-            }
-        });
+
 
         // Code to display the post content ready for updating
         updateBtn.setOnClickListener(new View.OnClickListener() {
@@ -58,6 +58,37 @@ public class UpdatePostActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_messenger) {
+            Intent intent = getPackageManager().getLaunchIntentForPackage("com.facebook.orca");
+            try {
+                startActivity(intent);
+            }
+            catch (android.content.ActivityNotFoundException ex){
+                Toast.makeText(getApplicationContext(), "Please Install Facebook Messenger", Toast.LENGTH_LONG).show();
+            }
+        } else if (id == R.id.action_logout) {
+            requestLogout();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void requestLogout() {
